2cefbfd798b168b5c4d47c5049218c1d3af5a7ee
busiso chisala <busiso.chisala@gmail.com>
2016-09-04 22:23:51 +0100

    still has abug - watching network now

--- pserv/main.go
+++ pserv/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"sync"
+	"time"
 
 	"coin/cpb"
 
@@ -14,8 +15,8 @@ import (
 )
 
 const (
-	port            = ":50051"
-	enoughGetWorkrs = 3
+	port          = ":50051"
+	enoughWorkers = 3
 )
 
 // server is used to implement cpb.CoinServer.
@@ -43,13 +44,13 @@ func (s *server) Login(ctx context.Context, in *cpb.LoginRequest) (*cpb.LoginRep
 }
 
 type starts struct {
-	linedUp int
-	waiting chan struct{}
+	countRunners int
+	theLine      chan struct{}
 }
 
 type starter struct {
 	mu sync.Mutex
-	in starts
+	on starts
 }
 
 var getSet starter
@@ -57,76 +58,88 @@ var getSet starter
 // GetWork implements cpb.CoinServer, synchronise start of miners
 func (s *server) GetWork(ctx context.Context, in *cpb.GetWorkRequest) (*cpb.GetWorkReply, error) {
 	fmt.Printf("GetWork req: %+v\n", in)
+
 	getSet.mu.Lock()
-	if getSet.in.linedUp == 0 {
-		fmt.Printf("GetWork lined=0, resetting getEnd.waiting ...\n")
-		getEnd.mu.Lock()
-		getEnd.waiting = make(chan struct{})
-		getEnd.mu.Unlock()
+	if getSet.on.countRunners == 0 { //fmt.Printf("GetWork lined=0, resetting waitFor.validStop ...\n")
+		waitFor.mu.Lock()
+		waitFor.validStop = make(chan struct{})
+		waitFor.mu.Unlock()
 	}
-	getSet.in.linedUp++
-	fmt.Printf("GetWork linedUp = %d\n", getSet.in.linedUp)
-	if getSet.in.linedUp == enoughGetWorkrs {
-		close(getSet.in.waiting)
-		getSet.in.linedUp = 0
+	getSet.on.countRunners++ //fmt.Printf("GetWork linedUp = %d\n", getSet.in.linedUp)
+	if getSet.on.countRunners == enoughWorkers {
+		close(getSet.on.theLine)
+		go getNewBlocks() // models watching the entire network, timeout our search
+		getSet.on.countRunners = 0
 	}
 	getSet.mu.Unlock()
 
 	work := fetchWork(in) // get this in advance
-	<-getSet.in.waiting   // wait
 
-	if getSet.in.linedUp == 0 { // we have just closed
+	<-getSet.on.theLine // wait
+
+	if getSet.on.countRunners == 0 { // we have just closed
 		getSet.mu.Lock()
-		getSet.in.waiting = make(chan struct{})
+		getSet.on.theLine = make(chan struct{})
 		getSet.mu.Unlock()
 	}
 	return &cpb.GetWorkReply{Work: work}, nil
 }
 
-// Announce implements cpb.CoinServer
+// Announce responds to a proposed solution : implements cpb.CoinServer
 func (s *server) Announce(ctx context.Context, soln *cpb.AnnounceRequest) (*cpb.AnnounceReply, error) {
-	checked := verify(soln)
+	fmt.Printf("verify solution: %+v\n", soln)
+	watchNet.mu.Lock()
+	if !weWonOutAlready() {
+		close(watchNet.weWon) // cancel previous getNewBlocks
+	}
+	watchNet.mu.Unlock()
+	endRun()
+	checked := true // TODO - accommodate possible mistaken solution
 	return &cpb.AnnounceReply{Ok: checked}, nil
 }
 
-// GetCancel implements cpb.CoinServer
+// GetCancel blocks until a valid stop condition then broadcasts a cancel instruction : implements cpb.CoinServer
 func (s *server) GetCancel(ctx context.Context, in *cpb.GetCancelRequest) (*cpb.GetCancelReply, error) {
-	fmt.Printf("GetCancel request from %s\n", in.Name)
+	// fmt.Printf("GetCancel request from %s\n", in.Name)
+	<-waitFor.validStop // wait for valid solution  OR timeout
 
-	<-getEnd.waiting // wait
-
-	fmt.Printf("GetCancel OUT: %s\n", in.Name)
+	fmt.Printf("OUT: %s\n", in.Name)
 	return &cpb.GetCancelReply{Ok: true}, nil
 }
 
-// check whether the proposed nonce/coinbase works with current block
-// TODO -this should return err as well
-func verify(soln *cpb.AnnounceRequest) bool {
-	// select {
-	// case <-getEnd.waiting:
-	// 	fmt.Printf("LATE proposed solution: %+v\n", soln)
-	// 	return true
-	// default:
-	fmt.Printf("received proposed solution: %+v\n", soln)
-	getEnd.mu.Lock()
-	select {
-	case <-getEnd.waiting: // already closed
-	default:
-		close(getEnd.waiting)
-	}
-	getEnd.mu.Unlock()
-	return true
-	// }
-}
-
 type stoper struct {
-	mu      sync.Mutex
-	waiting chan struct{}
+	mu        sync.Mutex
+	validStop chan struct{}
 }
 
-var getEnd stoper
+var waitFor stoper
 
-//TODO - move these out of here
+func endRun() {
+	waitFor.mu.Lock()
+	if !closedAlready() {
+		close(waitFor.validStop)
+	}
+	waitFor.mu.Unlock()
+	fmt.Printf("New race!\n--------------------\n")
+}
+
+func closedAlready() bool {
+	select {
+	case <-waitFor.validStop:
+		return true
+	default:
+		return false
+	}
+}
+
+func weWonOutAlready() bool {
+	select {
+	case <-watchNet.weWon:
+		return true
+	default:
+		return false
+	}
+}
 
 // prepares the candidate block and also provides user specific coibase data
 // TODO -this should return err as well
@@ -139,7 +152,36 @@ func init() {
 	users.loggedIn = make(map[string]int)
 	users.nextID = -1
 
-	getSet.in.waiting = make(chan struct{})
+	getSet.on.theLine = make(chan struct{})
+}
+
+type external struct {
+	mu    sync.Mutex
+	weWon chan struct{} // closed if we do win
+}
+
+var watchNet external
+
+func getNewBlocks() {
+	watchNet.weWon = make(chan struct{})
+	tick := time.Tick(1 * time.Second)
+	period := 37 // seconds after which we cancel search
+	for i := 0; i < period; i++ {
+		<-tick // continue, but check every second
+		//fmt.Printf("getnewblocks ... \n")
+		if func() bool {
+			select {
+			case <-watchNet.weWon:
+				return true
+			default:
+				return false
+			}
+		}() {
+			return // because we won, endRun called elsewhere
+		}
+	}
+	// otherwise reach this after period seconds
+	endRun()
 }
 
 func main() {
