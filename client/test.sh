1edb0c4d74fcd6ee3eda7c94243be09bec11a592
busiso chisala <busiso.chisala@gmail.com>
2016-09-13 09:47:52 +0100

    make test.sh take arguments

--- client/test.sh
+++ client/test.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-        for i in `seq 1 100`;
-        do
-                ./client -t 4 -u $i  &
-        done     
+
+# with no parameters - start 3 clients each with 2 tosses per roll
+# ./test.sh C  - start C clients .. 2 tosses
+# ./test.shh C T - start C clients ... T tosses 
+
+        if test $# -gt 0; then
+                COUNTER=$1
+        else
+                COUNTER=3
+        fi
+        TOSSES=2
+        shift
+        if test $# -gt 0; then
+                TOSSES=$1
+        fi
+        until [  $COUNTER -lt 1 ]; do
+                ./client -t $TOSSES -u $COUNTER  &
+                let COUNTER-=1
+        done
+        
+        # for i in `seq 1 100`;
+        # do
+        #         ./client -t 4 -u $i  &
+        # done     
