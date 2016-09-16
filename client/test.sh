6129621787d15582482fe5027e92dbbf152304bc
busiso chisala <busiso.chisala@gmail.com>
2016-09-16 13:31:58 +0100

    accommodate multi servers

--- client/test.sh
+++ client/test.sh
@@ -2,7 +2,8 @@
 
 # with no parameters - start 3 clients each with 2 tosses per roll
 # ./test.sh C  - start C clients .. 2 tosses
-# ./test.shh C T - start C clients ... T tosses 
+# ./test.shh C T  - start C clients ... T tosses 
+# ./test.shh C T I - start C clients ... T tosses  ... server I
 
         if test $# -gt 0; then
                 COUNTER=$1
@@ -14,8 +15,13 @@
         if test $# -gt 0; then
                 TOSSES=$1
         fi
+        INDEX=0
+        shift
+        if test $# -gt 0; then
+                INDEX=$1
+        fi
         until [  $COUNTER -lt 1 ]; do
-                ./client -t $TOSSES -u $COUNTER  &
+                ./client -t $TOSSES -u $COUNTER -s $INDEX &
                 let COUNTER-=1
         done
 
