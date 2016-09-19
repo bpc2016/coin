938d168f9e99c74532e82b885140aa701edfb8aa
busiso chisala <busiso.chisala@gmail.com>
2016-09-19 16:07:26 +0100

    confirmed changes  use workgates

--- server/server.go
+++ server/server.go
@@ -55,14 +55,14 @@ type locked struct {
 	ch chan struct{}
 }
 
-var tooLate locked
+var workgates locked
 
 // GetWork implements cpb.CoinServer, synchronise start of miners
 func (s *server) GetWork(ctx context.Context, in *cpb.GetWorkRequest) (*cpb.GetWorkReply, error) {
 	debugF("Work request: %+v\n", in)
 	s.signIn <- in.Name // register
-	// s.start.Wait()      // all must wait, start when this is Done()
-	<-tooLate.ch // all must wait, start when this is closed
+	<-workgates.ch      // all must wait, start when this is closed
+
 	block.Lock()
 	work := &cpb.Work{Coinbase: in.Name, Block: []byte(block.data)}
 	block.Unlock()
@@ -71,21 +71,19 @@ func (s *server) GetWork(ctx context.Context, in *cpb.GetWorkRequest) (*cpb.GetW
 
 // Announce responds to a proposed solution : implements cpb.CoinServer
 func (s *server) Announce(ctx context.Context, soln *cpb.AnnounceRequest) (*cpb.AnnounceReply, error) {
-	//won := s.vetWin(*soln.Win)
-	tooLate.Lock()
-	defer tooLate.Unlock()
-	if tooLate.on {
+	workgates.Lock()
+	defer workgates.Unlock()
+	if workgates.on {
 		return &cpb.AnnounceReply{Ok: false}, nil
 	}
 	// we have a winner
-	tooLate.on = true // until call for new run resets this one
+	workgates.on = true // until call for new run resets this one
 	resultchan <- *soln.Win
 	for i := 0; i < *numMiners; i++ {
 		<-s.signOut
 	}
-	tooLate.ch = make(chan struct{})
-	s.stop.Done() // issue cancellation to our clients
-	// s.start.Add(1) // reset getwork start
+	workgates.ch = make(chan struct{}) // reset getwork start
+	s.stop.Done()                      // issue cancellation to our clients
 	return &cpb.AnnounceReply{Ok: true}, nil
 }
 
@@ -97,27 +95,6 @@ func (s *server) GetCancel(ctx context.Context, in *cpb.GetCancelRequest) (*cpb.
 	return &cpb.GetCancelReply{Index: uint32(serv)}, nil
 }
 
-// vetWins handles wins - all are directed here
-// func (s *server) vetWin(thewin cpb.Win) bool {
-// 	s.search.Lock()
-// 	defer s.search.Unlock()
-// 	won := false
-// 	select {
-// 	case <-s.search.ch: // closed if already have a winner
-// 	default: // should vet the solution, return false otherwise!!
-// 		close(s.search.ch)   // until call for new run resets this one
-// 		resultchan <- thewin // fmt.Sprintf("Winner: %+v", thewin)
-// 		for i := 0; i < *numMiners; i++ {
-// 			<-s.signOut
-// 		}
-
-// 		s.stop.Done()  // issue cancellation to our clients
-// 		s.start.Add(1) // reset getwork start
-// 		won = true
-// 	}
-// 	return won
-// }
-
 type privchannel struct {
 	sync.Mutex
 	ch chan struct{}
@@ -125,10 +102,8 @@ type privchannel struct {
 
 // server is used to implement cpb.CoinServer.
 type server struct {
-	signIn  chan string    // for registering users in getwork
-	signOut chan string    // for registering leaving users in getcancel
-	search  privchannel    // search.ch is closed when we have dclared a winner
-	start   sync.WaitGroup // this is the tricky one, stop here for company
+	signIn  chan string // for registering users in getwork
+	signOut chan string // for registering leaving users in getcancel
 	stop    sync.WaitGroup
 }
 
@@ -172,13 +147,12 @@ func debugF(format string, args ...interface{}) {
 
 // initalise
 func init() {
-	users.loggedIn = make(map[string]int)
-	users.nextID = -1
 }
 
 func main() {
 	flag.Parse()
-
+	users.loggedIn = make(map[string]int)
+	users.nextID = -1
 	*numMiners++ // to include the EXTERNAL
 
 	port := fmt.Sprintf(":%d", 50051+*index)
@@ -189,27 +163,23 @@ func main() {
 
 	s.signIn = make(chan string, *numMiners)
 	s.signOut = make(chan string, *numMiners)
-	//s.start.Add(1) // get work start
-	tooLate.ch = make(chan struct{}) // prepare for getwork start
-
-	blockchan = make(chan string, 1) // buffered
+	workgates.ch = make(chan struct{}) // prepare for getwork start
+	blockchan = make(chan string, 1)
 	resultchan = make(chan cpb.Win)
 
 	go func() {
 		for {
-			tooLate.Lock()
-			tooLate.on = false
-			tooLate.Unlock()
+			workgates.Lock()
+			workgates.on = false
+			workgates.Unlock()
 			getNewBlock()
 			for i := 0; i < *numMiners; i++ { // loop blocks here until miners are ready
 				<-s.signIn
 			}
 
 			fmt.Printf("\n--------------------\nNew race!\n")
-			s.stop.Add(1)     // prep channel for getcancels
-			close(tooLate.ch) // start our miners
-			// s.search.ch = make(chan struct{}) // reset this channel
-			// s.start.Done() // start our miners
+			s.stop.Add(1)       // prep channel for getcancels
+			close(workgates.ch) // start our miners
 		}
 	}()
 
